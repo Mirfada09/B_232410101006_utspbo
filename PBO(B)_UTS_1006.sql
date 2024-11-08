290906594efb53ddf4eaf98f76a9f1bd6001344b
Mirfada09 <yoanamirfada@gmail.com>
2024-11-08 17:53:56 +0700

    Add files via upload

---
+++ PBO(B)_UTS_1006.sql
@@ -0,0 +1,23 @@
+
+CREATE TABLE login ( 
+    id SERIAL PRIMARY KEY, 
+    username VARCHAR(100) NOT NULL, 
+password INT NOT NULL
+); 
+
+CREATE TABLE admin ( 
+    id SERIAL PRIMARY KEY, 
+    nomor hp VARCHAR(100) NOT NULL, 
+nama INT NOT NULL, 
+email INT NOT NULL,
+alamat INT NOT NULL
+); 
+
+INSERT INTO login (id) VALUES
+
+INSERT INTO admin (nama_admin) VALUES  
+('Mirfada yoana'), 
+('kd0iyh');
+
+select * from admin
+select * from login 
